ba1f9ad5f796da3b9751bbf6b33e05c5ee170b0e
Simon Leykum <git@leykum.net>
2020-06-12 16:30:19 +0200

    remove unused import

--- src/me/simkon/permelytra/ElytraListener.java
+++ src/me/simkon/permelytra/ElytraListener.java
@@ -4,7 +4,6 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
